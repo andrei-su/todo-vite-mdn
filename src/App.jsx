48eca7efb1665de8d2b38c76bdb3da3bb8fea2c1
Andrei <git1@email.com>
2024-11-13 10:55:43 +0100

    Practice with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
+import PropTypes from 'prop-types'
 
-function App() {
+function App(props) {
   return (
     <>
       <header>
-        <h1>Hello, World!</h1>
+        <h1>
+          {props.greeting}, {props.subject}!
+        </h1>
+        <button type="button" className="primary">
+          Click me!
+        </button>
       </header>
     </>
   )
+}
+
+App.propTypes = {
+  subject: PropTypes.string.isRequired,
+  greeting: PropTypes.string.isRequired,
 }
 
 export default App
