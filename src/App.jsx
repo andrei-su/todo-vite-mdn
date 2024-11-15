0bdde037290efb7ff97b54a731157dd181eedcdb
Andrei <git1@email.com>
2024-11-15 13:19:14 +0100

    Rendering with iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Todo from './components/Todo'
+import PropTypes from 'prop-types'
 
 function App(props) {
+  const taskList = props.tasks?.map((task) => (
+    <Todo id={task.id} name={task.name} completed={task.completed} />
+  ))
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -44,12 +49,20 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name="Eat" id="todo-0" completed />
-        <Todo name="Sleep" id="todo-1" />
-        <Todo name="Repeat" id="todo-2" />
+        {taskList}
       </ul>
     </div>
   )
+}
+
+App.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      completed: PropTypes.bool,
+    })
+  ),
 }
 
 export default App
